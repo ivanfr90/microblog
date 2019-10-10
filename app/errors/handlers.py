a45830bd87585ff53c42cf162b09b9e4b326056f
ivanfr90 <ivan.fr1990@gmail.com>
2019-10-11 01:14:09 +0200

    Fixed bug with error handlers. Now using specific error hander for blueprints

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -4,11 +4,11 @@ from app.extensions import db
 from app.errors import errors
 
 
-@errors.errorhandler(404)
+@errors.app_errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
 
-@errors.errorhandler(500)
+@errors.app_errorhandler(500)
 def internal_error(error):
     db.session.rollback()
     return render_template('500.html'), 500
